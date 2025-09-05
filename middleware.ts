5b8e51015c8f21e50ed4ef41e9588a730986674a
mihir <mihirsoni8510@gmail.com>
2025-09-05 11:32:46 +0530

    Fix/ Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,19 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb-access-token')?.value
-  const { pathname } = req.nextUrl
+  const token = req.cookies.get("sb-access-token")?.value;
+  const { pathname } = req.nextUrl;
 
-  if (!token && pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/', req.url))
+  if (!token && pathname.startsWith("/dashboard")) {
+    if (pathname !== "/") {
+      return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+    }
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
-}
+  matcher: ["/dashboard/:path*"],
+};
